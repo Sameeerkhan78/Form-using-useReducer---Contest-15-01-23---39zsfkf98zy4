8705065a314d3b62505ed70a6ec88097fc50c25a
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2023-01-15 21:34:46 +0530

    Update FormUsingReducer.js

--- src/components/FormUsingReducer.js
+++ src/components/FormUsingReducer.js
@@ -1,3 +1,4 @@
+
 import React,{ useReducer } from "react"
 import { signUpFormReducer } from "../reducers/signUpFormReducer"
 import { signUpFormValidation } from "../utils/signupformvalidation"
@@ -17,49 +18,49 @@ const initialFormState = {
 }
 const FormUsingReducer = () => {
     const [formState,dispatch] = useReducer(signUpFormReducer,initialFormState) 
-        const onInput =(e) =>{
-        dispatch({type:'input', payload:{field:e.target.id, value:e.target.value}})
-        }
-        
-        
-        const onFormSubmit =(e)=>{
-            console.log('enter');
-            e.preventDefault()
-            dispatch({type:'resetErrors'})
-            const errors = signUpFormValidation(formState.input)
-                if(errors !==null){
-                console.log(errors);
-                for(const i in errors){
-                    dispatch(type:'error', payload:{field:i, value:errors[i]})
-                }
-                }else{
-                    alert('form submitted succesfully...')
-                }
-        }
 
+    const onInput = (e) =>{
+        //console.log({type:'input',payload:{field:e.target.id,value:e.target.value}});
+        dispatch({type:'input',payload:{field:e.target.id,value:e.target.value}})
+    }
+    const onFormSubmit = (e) =>{
+        console.log('entette');
+        e.preventDefault()
+        dispatch({type:'resetErrors'})
+        const errors = signUpFormValidation(formState.input)
+        if(errors !== null){
+            console.log(errors);
+            for (const m in errors){
+                dispatch({type:'error',payload:{field:m,value:errors[m]}})
+            }
+        } else {
+            alert('form submitted successfully!!')
+        }
+    }
+    
     return (
         <form id="reducer-form" onSubmit={onFormSubmit}>
-            <label htmlFor="name">Name </Label>
-            <input type="text" id="name" value={formState["name"]} onChange={onInput}/>
-                {formState.errors["name"]}
-                </br>
-                </br>
-            <label htmlFor="password">Password</Label>
-            <input type="text" id="password" value={formState["password"]}  onChange={onInput}/>
-                {formState.errors["password"]}
-                </br>
-                </br>
-             <label htmlFor="email ">Email</Label>
-            <input type="text" id="email" value={formState["email"]}  onChange={onInput}/>
-                {formState.errors["email"]}
-                </br>
-                </br>
-            <label htmlFor="consent">Send marketing emails?</Label>
-            <input type="checkbox" id="consent" checked={formState.consent}/>
-                
-              <button type="submit">Sign Up</button>
+            <label htmlFor="name">Name </label>
+            <input type="text" id="name" value={formState["name"]} onChange={onInput} />
+            {formState.errors["name"]}
+            <br />
+            <br />
+            <label htmlFor="password">Password </label>
+            <input type="text" id="password"  value={formState["password"]} onChange={onInput}/>
+            {formState.errors["password"]}
+            <br />
+            <br />
+            <label htmlFor="email">Email </label>
+            <input type="text" id="email" value={formState["email"]} onChange={onInput} />
+            {formState.errors["email"]}
+            <br />
+            <br />
+            <label htmlFor="consent">Send marketing emails? </label>
+            <input type="checkbox" id="consent" checked={formState.consent} />
+            <button type="submit">Sign Up</button>
         </form>
     )
 }
 
 export { FormUsingReducer }
+   
